b20f0f6cf14d35d4689df25444afa679feab3b4c
kunal jaiswal <kunaljaiswal262358@gmail.com>
2025-03-04 13:23:10 +0530

    added environment variable

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,10 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
+require("dotenv").config()
 
 module.exports = function() {
-  const db = config.get('db');
+  const db = process.env.vidlydb || config.get('db');
   mongoose.connect(db)
     .then(() => winston.info(`Connected to ${db}...`));
 }
